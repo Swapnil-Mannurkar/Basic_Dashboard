3299578761c1f80290936ea3c6bac8378bb3c78d
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-08-22 16:26:49 +0530

    commit

--- src/components/loginPage/Login.jsx
+++ src/components/loginPage/Login.jsx
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
 import "./Login.css";
 import Logo from "./logo/Logo";
 import LoginOptions from "./loginOptions/LoginOptions";
 
 const Login = (props) => {
+  const [displayForm, setDisplayForm] = useState(false);
+
+  const animationCompleteHandler = () => setDisplayForm(!displayForm);
+
+  const loginPage = {
+    transition: "all 0.5s ease",
+    gridTemplateColumns: `${displayForm ? "42% 58%" : "100% 0%"}`,
+  };
+
   return (
-    <div className="loginPage">
-      <Logo />
-      <LoginOptions onClick={props.onClick} />
+    <div className="loginPage" style={loginPage}>
+      <Logo onAnimationComplete={animationCompleteHandler} />
+      {displayForm && <LoginOptions onClick={props.onClick} />}
     </div>
   );
 };
--- src/components/loginPage/logo/Logo.jsx
+++ src/components/loginPage/logo/Logo.jsx
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from "react";
 import "./Logo.css";
 
-const Logo = () => {
+const Logo = (props) => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    const animationTimeout = setTimeout(() => {
+      setAnimationComplete(true);
+      props.onAnimationComplete();
+    }, 2000);
+
+    return () => 
+      clearTimeout(animationTimeout);
+
+  }, []);
+
+  const logoName = {
+    animation: "fadeInAnimation ease 3s",
+  };
+
   return (
     <div className="logoContainer">
-      <p className="logoName">Board.</p>
+      <p className="logoName" style={logoName}>
+        Board.
+      </p>
     </div>
   );
 };
