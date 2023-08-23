017f0a25946967dd6110d35eb9ebcd5ab43497a4
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-08-23 10:23:48 +0530

    commit

--- src/components/loginPage/loginOptions/LoginForm.jsx
+++ src/components/loginPage/loginOptions/LoginForm.jsx
@@ -1,11 +1,16 @@
+import React, { useRef } from "react";
 import Button from "../../UI/Button";
 import "./LoginForm.css";
 
 const LoginForm = () => {
+  const emailInput = useRef();
+  const passwordInput = useRef();
+
   return (
     <form action="" className="loginForm">
       <label htmlFor="emailAddress">Email Address</label>
       <input
+        ref={emailInput}
         type="email"
         className="loginFormInput"
         placeholder="johndoe@gmail.com"
@@ -13,7 +18,12 @@ const LoginForm = () => {
       <label htmlFor="password" style={{ marginTop: "10px" }}>
         Password
       </label>
-      <input type="password" className="loginFormInput" />
+      <input
+        ref={passwordInput}
+        placeholder="Password"
+        type="password"
+        className="loginFormInput"
+      />
       <a href="/" className="forgotPasswordLink" style={{ margin: "5px 0" }}>
         Forgot password?
       </a>
